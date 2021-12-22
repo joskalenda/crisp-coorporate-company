64e4a383e1f952403646fedba3cf0b81214b0fff
joskalenda <joskalenda3@gmail.com>
2021-12-23 01:40:26 +0200

    Create nemu.js and update code to it

---
+++ JS/menu.js
@@ -0,0 +1,84 @@
+const Speaker = document.querySelector('.feature--container');
+
+const projects = [
+  {
+    id: 'member1',
+    images: {
+      img: 'assets/backgrd/member1.png',
+      altText: 'member 1',
+    },
+    name: 'Yochai Benkler',
+    heading: 'Founder and CEO of Crisp Company',
+    description: 'Crisp has been an incredible and invaluable part of our team and really protected our users.'
+  },
+  {
+    id: 'project_2',
+    images: {
+      img: 'assets/backgrd/member2.png',
+      altText: 'member 2',
+    },
+    name: 'Rachid Hammas',
+    heading: 'Founder and CEO of Crisp Company',
+    description: 'Crisp has been an incredible and invaluable part of our team and really protected our users.'
+  },
+  {
+    id: 'project_3',
+    images: {
+      img: 'assets/backgrd/member3.png',
+      altText: 'member 3',
+    },
+    name: 'Petter Benkler',
+    heading: 'Founder and CEO of Crisp Company',
+    description: 'Crisp has been an incredible and invaluable part of our team and really protected our users.'
+  },
+  {
+    id: 'project_4',
+    images: {
+      img: 'assets/backgrd/member4.png',
+      altText: 'member 4',
+    },
+    name: 'Karis Yoga',
+    heading: 'Founder and CEO of Crisp Company',
+    description: 'Crisp has been an incredible and invaluable part of our team and really protected our users.'
+  },
+  {
+    id: 'project_4',
+    images: {
+      img: '/assets/backgrd/member5.png',
+      altText: 'member 5',
+    },
+    name: 'Julias Eduard',
+    heading: 'Founder and CEO of Crisp Company',
+    description: 'Crisp has been an incredible and invaluable part of our team and really protected our users.'
+  },
+  {
+    id: 'project_6',
+    images: {
+      img: 'assets/backgrd/member6.png',
+      altText: 'member 6',
+    },
+    name: 'David Beket',
+    heading: 'Founder and CEO of Crisp Company',
+    description: 'Crisp has been an incredible and invaluable part of our team and really protected our users.'
+}];
+
+function createSpreaker(Speakerobject) {
+  const li = document.createElement('li');
+  li.className = 'feature';
+  li.innerHTML = 
+  `<div class="feature--img hidden">
+    <img src="${Speakerobject.images.img}" alt="${Speakerobject.images.altText}">
+  </div>
+  <div class="side-div">
+    <h3>${Speakerobject.name}</h3>
+    <h4>${Speakerobject.heading}</h4>
+    <p>${Speakerobject.description}</p>
+  </div>`;
+
+  return li;
+}
+
+projects.forEach((object) => {
+  const Card = createSpreaker(object);
+  Speaker.appendChild(Card);
+});
