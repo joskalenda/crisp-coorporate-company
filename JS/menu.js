e688318a953950025ad32d33a21222a4a8a1d142
joskalenda <joskalenda3@gmail.com>
2021-12-23 03:41:06 +0200

    Final touch

--- JS/menu.js
+++ JS/menu.js
@@ -117,3 +117,16 @@ function Read() {
 }
 
 Button.addEventListener('click', Read);
+
+// header background
+
+const header = document.querySelector('.header');
+
+window.onscroll = function(){
+  let top = window.scrollY;
+  if (top >= 100){
+    header.classList.add('active');
+  }else{
+    header.classList.remove('active');
+  }
+}
